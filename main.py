0308b538c957d07c441e3318782944d5bc4a07d1
CarlileC <cjcarlile038@gmail.com>
2024-12-04 15:19:28 -0600

    Voting Form created

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+
+from flask_migrate import Migrate
 from databaseModels import db
 from flask import Flask, render_template, redirect, url_for, request
 
@@ -11,6 +13,16 @@ app.config["SECRET_KEY"] = "TEMP KEY"
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 db.init_app(app)
+Migrate(app, db)
 
 with app.app_context():
     db.create_all()
+    
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
