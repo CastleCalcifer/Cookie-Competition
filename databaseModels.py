8288137f001f3517d29e9de5e357cc12a2e731fb
CarlileC <cjcarlile038@gmail.com>
2024-12-05 10:32:13 -0600

    Baker schema

--- databaseModels.py
+++ databaseModels.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
 
-db = SQLAlchemy()
+db:SQLAlchemy = SQLAlchemy()
 
 class Cookie(db.Model):
     __tablename__="Cookie"
@@ -9,6 +9,7 @@ class Cookie(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     cookie_name = db.Column(db.Text)
     score = db.Column(db.Integer)
+    baker_id = db.Column(db.Integer, db.ForeignKey('Baker.id'))
     
     def __init__(self, cookie_name, score):
         self.cookie_name = cookie_name
@@ -17,4 +18,10 @@ class Cookie(db.Model):
     def __repr__(self):
         return f"ID: {self.id} cookie_name: {self.cookie_name} score: {self.score}"
     
+class Baker(db.Model):
+    __tablename__ = "Baker"
     
+    id = db.Column(db.Integer, primary_key = True)
+    baker_name = db.Column(db.Text)
+    hasVoted = db.Column(db.Boolean)
+    cookie_id = db.Column(db.Integer, db.ForeignKey('Cookie.id'))
