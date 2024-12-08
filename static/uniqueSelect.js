0204ec49914a3a972cfaa1caafc514d7d9b201a6
CarlileC <cjcarlile038@gmail.com>
2024-12-08 09:37:44 -0600

    added Array.from to the HTML-likes

--- static/uniqueSelect.js
+++ static/uniqueSelect.js
@@ -1,10 +1,10 @@
 const selectedOptions = [];
 document.querySelector('form').addEventListener('change', e => { //waits for change in the form
-  const selects = document.querySelectorAll('select'); //grabs all drop downs
+  const selects = Array.from(document.querySelectorAll('select')); //grabs all drop downs
   selectedOptions[e.target.id] = e.target.value; //puts the current ranking in a selected list
   [...selects].forEach(select => { //iterates over the 5 drop down menus
     if(e.target.id !== select.id) { // targets the other 4 drop down menus
-      const options = select.getElementsByTagName('option'); // grabs the options from each drop down
+      const options = Array.from(select.getElementsByTagName('option')); // grabs the options from each drop down
       [...options].forEach((option) => { //iterates over each option in the select field
         if(selectedOptions.includes(option.value) && option.value !== "---"){ //if the select field is the just select option, but not the default "---" hide it
           option.style.display = "none";
